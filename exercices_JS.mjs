cd6c498d92285f2ccb3304aebd2c10723fddd58c
Kilian Testard <pc47xiy@eduvaud.ch>
2025-08-19 15:45:55 +0200

    feat: exercises 1-3

---
+++ exercices_JS.mjs
@@ -0,0 +1,55 @@
+
+/* Exercise n°1 19.08.2025
+let musics_total = 125;
+console.log(musics_total);
+musics_total -= 15;
+console.log(musics_total);
+musics_total += 10;
+console.log(musics_total)
+console.log("Now there are " + musics_total + " tracks in total in my playlist"); */
+
+
+/* Exercise 2 19.08.2025
+let playlist = [];
+let track1 = "Sultans of swing";
+let track2 = "Highway star";
+let track3 = "Stairway to Heaven";
+playlist.push(track1, track2, track3);
+let track4 = "You give love a bad name";
+let track5 = "Detroit Rock City";
+playlist.push(track4, track5);
+playlist.splice(3, 1);
+playlist.splice(3, 1);
+playlist.splice(7, 1);
+function display_playlist ()
+    {console.log(`My playlist contains ${playlist.length} tracks :  ${playlist}`)}
+display_playlist() */
+
+
+/* Exercise 3 method 1 19.08.2025
+for (let i = 1; i < 10; ++i) {
+    console.log(String(i).repeat(i))
+} */
+/* Exercise 3 method 2 19.08.2025
+for (let i =1; i < 10; ++i) {
+    let repeat = "";
+    for (let j=0; j < i; ++j) {
+        repeat += i;
+    }
+    console.log(repeat);
+} */
+
+
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+
+
+
+
+
+
+
+
