d6a4e60018286b265154e86f7971db7becc1f83f
Kilian Testard <pc47xiy@eduvaud.ch>
2025-09-04 10:09:55 +0200

    week 3 commit

--- exercices_JS.mjs
+++ exercices_JS.mjs
@@ -40,10 +40,50 @@ for (let i =1; i < 10; ++i) {
 } */
 
 
+/* Exercise 4 25.08.2025
 const add = (a, b) => a + b;
-const substract = (a, b) => a - b;
+const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide = (a, b) => {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero !")}
+    return a / b};
+
+console.log("Your result is : " + add(4, 5));
+console.log("Your result is : " + subtract(7, 3));
+console.log("Your result is : " + multiply(4, 2));
+console.log("Your result is : " + divide(9, 3));
+try {
+    console.log("Your result is : " + divide(5, 0))}
+catch (error) {
+    console.error("Error: ", error.message)} */
+
+
+/* Exercise 5 25.08.2025
+import dayjs from "dayjs";
+let now =dayjs()
+const person = {
+    firstname: "Jean",
+    lastname: "Dupont",
+    birthdate: '1921-08-07',
+    power: "immortality",
+    display: function () {
+        const bd = dayjs(this.birthdate)
+        const age = now.diff(bd, "year")
+        if (age > 100 && this.power === "immortality") {
+            return ("How incredible !")
+        }
+        else {
+            return `${this.firstname} ${this.lastname} is ${age} years old and has ${this.power} as their power.`;
+        }
+    }
+}
+
+console.log(person.firstname);
+console.log(person.lastname);
+console.log(person.birthdate);
+console.log(person.power);
+console.log(person.display()); */
 
 
 
